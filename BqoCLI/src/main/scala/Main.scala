8a6e179a7ec31cacfcbfa8dffe0d6d52fbbf7110
Edward Wang <edward.c.wang@compdigitec.com>
2025-07-01 21:01:36 -0400

    Fix cents conversion function

--- BqoCLI/src/main/scala/Main.scala
+++ BqoCLI/src/main/scala/Main.scala
@@ -29,7 +29,7 @@ object Utils {
   def intCentsToString(total: Long): String = {
     val cents = (total.abs % 100)
     val centsStr: String = (if (cents < 10) "0" else "") + cents.toString
-    val amount = (total / 100).toString + "." + centsStr
+    val amount = (if (total < 0) "-" else "") + (total.abs / 100).toString + "." + centsStr
     amount
   }
 
